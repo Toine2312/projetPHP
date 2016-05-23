60f87cb5ec2e13b60f604522f5cd7db8e35b4dd0
Antoine Perot <antoine.perot@etu.iut-tlse3.fr>
2016-05-23 11:48:54 +0200

    ModifPatient

--- Patient.php
+++ Patient.php
@@ -6,84 +6,51 @@
 </head>
 <body>
 	<H1><center>Carnet de Patient</center></H1>
-	<form action="ajoutPatient.php" method="POST"  id="ajoutPatient">
-		<fieldset><legend><h1> Ajouter un patient</h1></legend>
-			<table>
-				<tr>
-					<td>Civilit&eacute;</td>
-					<td> <select name="civilite"><option value="M." selected="selected">M.</option><option value="Mme.">Mme.</option></select></td>
-				</tr>
-				<tr>
-					<td>Nom</td>					
-					<td> <input type="text" name="nom"></td>
-				</tr>
-				<tr>
-					<td>Prenom</td>
-					<td> <input type="text" name="prenom"></td>
-				</tr>
-				<tr>
-					<td>Adresse</td>
-					<td> <input type="text" name="adresse"></td>
-				</tr>
-				<tr>
-					<td>Code postal</td>
-					<td> <input type="text" name="cp"></td>
-				</tr>
-				<tr>
-					<td>Ville</td>
-					<td> <input type="text" name="ville"></td>
-				</tr>
-				<tr>
-					<td>Date de Naissance</td>
-					<td> <input type="date" name="dateNaissance"></td>
-				</tr>
-				<tr>
-					<td>Lieu de Naissance</td>
-					<td> <input type="text" name="lieuNaissance"></td>
-				</tr>
-				<tr>
-					<td>Numero Securit&eacute; Social</td>
-					<td> <input type="text" name="numSS"></td>
-				</tr>
-				<tr>
-					<td colspan=2 style='text-align:center;'><input type="submit" value="Ajouter"></td>
-				</tr>
-			</table>
-		</fieldset>
-	</form>
-	<aside>
-		<table align="center" bgcolor="D8D8D8" style="border-radius: 10px">
-			<tr>
-				<td style='text-align:center;'>Civilit&eacute;</td>
-				<td style='text-align:center;'>Nom</td>
-				<td style='text-align:center;'>Prenom</td>
-				<td style='text-align:center;'>Adresse</td>
-				<td style='text-align:center;'>Code postal</td>
-				<td style='text-align:center;'>Ville</td>
-				<td style='text-align:center;'>Date de Naissance</td>
-				<td style='text-align:center;'>Lieu de Naissance</td>
-				<td style='text-align:center;'>Numero Securit&eacute; Social</td>
-			</tr>
-			<?php
-			include ("./ConnexionBDD.php");
-			$req=$linkpdo->query('Select * from patient');
-			while ($res=$req->fetch()) {
-			echo "	<tr>
-					<td>".$res['civiliteP']."</td>
-						<td>".$res['nomP']."</td>
-						<td>".$res['prenomP']."</td>
-						<td>".$res['adresseP']."</td>
-						<td>".$res['cpP']."</td>
-						<td>".$res['villeP']."</td>
-						<td>".$res['dateNaissanceP']."</td>
-						<td>".$res['lieuNaissanceP']."</td>
-						<td>".$res['numSS']."</td>
-						<td style='text-align:center;'><a href='modifierPatient.php?idPatient=".$res['idPatient']."'><IMG src='modifier.png' alt='Modifier ce contact'></a></td>
-						<td style='text-align:center;'><a href='supprimerPatient.php?idPatient=".$res['idPatient']."'><IMG src='supprimer.png' alt='Supprimer ce contact'></a></td>
-					</tr>";
-			}
+	<table align="center" bgcolor="D8D8D8" style="border-radius: 10px">
+		<tr>
+			<td style='text-align:center;'>Civilit&eacute;</td>
+			<td style='text-align:center;'>Nom</td>
+			<td style='text-align:center;'>Prenom</td>
+			<td style='text-align:center;'>Adresse</td>
+			<td style='text-align:center;'>Code postal</td>
+			<td style='text-align:center;'>Ville</td>
+			<td style='text-align:center;'>Date de Naissance</td>
+			<td style='text-align:center;'>Lieu de Naissance</td>
+			<td style='text-align:center;'>Numero Securit&eacute; Social</td>
+		</tr>
+		<?php
+		include ("./ConnexionBDD.php");
+		$req=$linkpdo->query('Select * from patient');
+		while ($res=$req->fetch()) {
+		echo "	<tr>
+				<td>".$res['civiliteP']."</td>
+					<td>".$res['nomP']."</td>
+					<td>".$res['prenomP']."</td>
+					<td>".$res['adresseP']."</td>
+					<td>".$res['cpP']."</td>
+					<td>".$res['villeP']."</td>
+					<td>".$res['dateNaissanceP']."</td>
+					<td>".$res['lieuNaissanceP']."</td>
+					<td>".$res['numSS']."</td>
+					<td style='text-align:center;'><a href='modifierPatient.php?idPatient=".$res['idPatient']."'><IMG src='modifier.png' alt='Modifier ce contact'></a></td>
+					<td style='text-align:center;'><a href='supprimerPatient.php?idPatient=".$res['idPatient']."'><IMG src='supprimer.png' alt='Supprimer ce contact'></a></td>
+				</tr>";
+		}
 		?>
-		</table>
-	</aside>
+		<form action="ajoutPatient.php" method="POST"  id="ajoutPatient">
+			<tr>
+				<td> <select name="civilite"><option value="M." selected="selected">M.</option><option value="Mme.">Mme.</option></select></td>					
+				<td> <input type="text" name="nom"></td>
+				<td> <input type="text" name="prenom"></td>
+				<td> <input type="text" name="adresse"></td>
+				<td> <input type="text" name="cp"></td>
+				<td> <input type="text" name="ville"></td>
+				<td> <input type="date" name="dateNaissance"></td>
+				<td> <input type="text" name="lieuNaissance"></td>
+				<td> <input type="text" name="numSS"></td>
+				<td colspan=2 style='text-align:center;'><input type="submit" value="Ajouter"></td>
+			</tr>
+		</form>
+	</table>
 </body>
 </HTML>
