7c3a77ba1529f278d74f3b7049a2c2737529cf53
fannouIUT <fanny.daudies@etu.iut-tlse3.fr>
2016-05-23 10:01:47 +0200

    vous

--- accueil.php
+++ accueil.php
@@ -4,6 +4,6 @@
 		<link rel="stylesheet" type="text/css" href="style.css" media="all"/>
 	</head>
 	<body>
-		<center><h1>Connecter VOUS pour acceder à toutes les fonctionalités</h1></center>
+		<center><h1>Connecter vous pour acceder à toutes les fonctionalités</h1></center>
 	</body>
 </html>
