dcd6a96aff531a09a116c88b2e0c5b9e4d346e5d
fannouIUT <fanny.daudies@etu.iut-tlse3.fr>
2016-05-30 22:29:48 +0200

    rdv

--- rdv.php
+++ rdv.php
@@ -1,10 +1,39 @@
+
 <!DOCTYPE HTML>
 <html>
 	<head>	
 		<link rel="stylesheet" type="text/css" href="style.css" media="all"/>
+		<link rel="stylesheet" type="text/css" href="rdv.css" media="all"/>
 	</head>
 	<body>
 		<div class="container">
+			<?php
+				include ("./ConnexionBDD.php");
+				if(isset($_POST["ajout"])){
+					echo "ok".$_POST["patientSelect"];
+					$req1 = $linkpdo -> prepare ('INSERT into rdv (idPatient, idMed, dateRdv, heure, duree) values (:idPatient, :idMed, :dateRdv, :heure, :duree)');
+					$req1 -> execute(array(	'idPatient'=>$_POST["patientSelect"], 
+											'idMed'=>$_POST["medecin"], 
+											'dateRdv'=>$_POST["dateRdv"], 
+											'heure'=>$_POST["heure"], 
+											'duree'=>$_POST["duree"]));
+				}
+				if(isset($_POST["idRdvModif"])){
+					if(isset($_POST["validerModif".$_POST["idRdvModif"]])){
+						
+						$req2 = $linkpdo->prepare ('UPDATE rdv SET idPatient= :idPatient, idMed= :idMed, dateRdv= :dateRdv, heure= :heure, duree= :duree WHERE idRdv= :idRdv');
+						//echo "nvelle date = ".$_POST["dateModif"];
+						//echo "idRdv = ".$_POST["idRdvModif"];
+						$req2->execute(array(	'idPatient'=>$_POST["patientModif"], 
+												'idMed'=>$_POST["medecinModif"], 
+												'dateRdv'=>$_POST["dateModif"], 
+												'heure'=>$_POST["heureModif"], 
+												'duree'=>$_POST["dureeModif"],
+												'idRdv'=>$_POST["idRdvModif"]));
+					}
+				}
+				
+			?>
 			<div id="formAjout">
 				<fieldset>
 					<legend>Ajouter une consultation</legend>
@@ -68,9 +97,10 @@
 							</tr>
 							<tr>
 								<td></td>
-								<td></td>
+								<td><input type="hidden" name="patientSelect" <?php echo " .0value='".$patient."'"?>></td>
 								<td><input type="submit" name="ajout" value="Ajouter"></td>
-							</tr
+							</tr>
+							
 						</form>
 					</table>
 				</fieldset>
@@ -93,12 +123,12 @@
 						<form action='./?consult=ok' method='POST' id=".$i.">
 							<input type='hidden' name='idRdvModif' <?php echo "value='".$data['idRdv']."'"?>>
 							<td><input type = 'text' name='patientModif' <?php echo "value='".$data['idPatient']."'"?> <?php $val="modifier".$data["idRdv"]; if(!isset($_POST[$val])){echo "readonly";}?>></input></td>
-							<td><input type = 'text' name='medecinModif' readonly <?php echo "value='".$data['idMed']."'"?>></input></td>
-							<td><input type = 'text' name='dateModif' readonly <?php echo "value='".$data['dateRdv']."'"?>></td>
-							<td><input type = 'text' name='heureModif' readonly <?php echo "value='".$data['heure']."'"?>></td>
-							<td><input type = 'text' name='dureeModif'readonly <?php echo "value='".$data['duree']."'"?>></td>
-							<td><input src='modifier.png' type='submit' value='modifier' align='middle'<?php echo "name = 'modifier".$data["idRdv"]."'"?>></input></td>
-							<td><input src='croix.png' type='submit'  value='supprimer'align='middle' name = 'supprimer'></input></td>
+							<td><input type = 'text' name='medecinModif' <?php echo "value='".$data['idMed']."'"?><?php $val="modifier".$data["idRdv"]; if(!isset($_POST[$val])){echo "readonly";}?>></input></td>
+							<td><input type = 'date' name='dateModif'  <?php echo "value='".$data['dateRdv']."'"?> <?php $val="modifier".$data["idRdv"]; if(!isset($_POST[$val])){echo "readonly";}?>></td>
+							<td><input type = 'time' name='heureModif' <?php echo "value='".$data['heure']."'"?> <?php $val="modifier".$data["idRdv"]; if(!isset($_POST[$val])){echo "readonly";}?>></td>
+							<td><input type = 'time' name='dureeModif' <?php echo "value='".$data['duree']."'"?> <?php $val="modifier".$data["idRdv"]; if(!isset($_POST[$val])){echo "readonly";}?>></td>
+							<td><input type='submit' <?php $val="modifier".$data["idRdv"]; echo (!isset($_POST[$val])) ? 'class="btn_modifier"' : 'class="btn_validerModif"';?> value=" " align='middle'<?php echo (!isset($_POST[$val])) ? "name = 'modifier".$data["idRdv"]."'" : "name = 'validerModif".$data["idRdv"]."'"?>></input></td>
+							<td><input class="btn_supprimer" type='submit'  value=" " align='middle' name = 'supprimer'></input></td>
 						</form>
 					</tr>
 			<?php
@@ -108,17 +138,7 @@
 				///Fermeture du curseur d'analyse des résultats
 			   	$res->closeCursor(); 
 			?>
-			<?php
-				//include ("./ConnexionBDD.php");
-				if(isset($_POST["ajout"])){
-					$req = $linkpdo -> prepare ('INSERT into rdv (idPatient, idMed, dateRdv, heure, duree) values (:idPatient, :idMed, :dateRdv, :heure, :duree)');
-					$req -> execute(array(	'idPatient'=>$patient, 
-											'idMed'=>$_POST["medecin"], 
-											'dateRdv'=>$_POST["dateRdv"], 
-											'heure'=>$_POST["heure"], 
-											'duree'=>$_POST["duree"]));
-				}
-			?>
+			
 		</div>	
 	</body>
 </html>
