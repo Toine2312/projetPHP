1b0a9601e5e6fd70dbf5ef50e4afe1b46b1fef4d
fannouIUT <fanny.daudies@etu.iut-tlse3.fr>
2016-06-03 08:10:37 +0200

    02/06/2016

--- statistiques.php
+++ statistiques.php
@@ -1,12 +1,6 @@
 <!DOCTYPE>
-<HTML>
-<head>
-<link rel="stylesheet" href="Stat.css" />
-<link rel="stylesheet" type="text/css" href="style.css" media="all"/>
-<TITLE>Carnet de Patient</TITLE>
-</head>
-<body>
-	<div class="container">
+
+	<div class="pageInterne">
 		<h1><center>Statistique du cabinet</center></h1>
 		<br>
 		<h2>Repartition demographique des patients</h2>
@@ -22,6 +16,7 @@
 					<?php $req=$linkpdo->query("Select count(*) as nbM25 from patient where civiliteP='M.' and DATEDIFF(CURDATE(), `dateNaissanceP`) < '9131.25'");
 						if($res=$req->fetch()) {
 							echo $res['nbM25'];
+							$nbM25 = $res['nbM25'];
 						}
 					?>
 				</td>
@@ -29,6 +24,7 @@
 					<?php $req=$linkpdo->query("Select count(*) as nbMme25 from patient where civiliteP='Mme.' and DATEDIFF(CURDATE(), `dateNaissanceP`) < '9131.25'");
 						if($res=$req->fetch()) {
 							echo $res['nbMme25'];
+							$nbMme25 =$res['nbMme25'];
 						}
 					?>
 				</td>
@@ -39,6 +35,7 @@
 					<?php $req=$linkpdo->query("Select count(*) as nbM2550 from patient where civiliteP='M.' and DATEDIFF(CURDATE(), `dateNaissanceP`) > '9131.25' and DATEDIFF(CURDATE(), `dateNaissanceP`) < '18262.5'");
 						if($res=$req->fetch()) {
 							echo $res['nbM2550'];
+							$nbM2550 = $res['nbM2550'];
 						}
 					?>
 				</td>
@@ -46,6 +43,7 @@
 					<?php $req=$linkpdo->query("Select count(*) as nbMme2550 from patient where civiliteP='Mme.' and DATEDIFF(CURDATE(), `dateNaissanceP`) > '9131.25' and DATEDIFF(CURDATE(), `dateNaissanceP`) < '18262.5'");
 						if($res=$req->fetch()) {
 							echo $res['nbMme2550'];
+							$nbMme2550 = $res['nbMme2550'];
 						}
 					?>
 				</td>
@@ -56,6 +54,7 @@
 					<?php $req=$linkpdo->query("Select count(*) as nbM50 from patient where civiliteP='M.' and DATEDIFF(CURDATE(), `dateNaissanceP`) > '18262.5'");
 						if($res=$req->fetch()) {
 							echo $res['nbM50'];
+							$nbM50 = $res['nbM50'];
 						}
 					?>
 				</td>
@@ -63,6 +62,7 @@
 					<?php $req=$linkpdo->query("Select count(*) as nbMme50 from patient where civiliteP='Mme.' and DATEDIFF(CURDATE(), `dateNaissanceP`) > '18262.5'");
 						if($res=$req->fetch()) {
 							echo $res['nbMme50'];
+							$nbM50 = $res['nbM50'];
 						}
 					?>
 				</td>
@@ -88,23 +88,33 @@
 					}
 			?>
 		</table>
-	</div>
-</body>
-</HTML>
 
-<?php
-require_once("../conf.php");
-?>
-<!DOCTYPE HTML>
-<html>
-    <head>
-		<title>phpChart - Pie Chart 3</title>
-    </head>
-    <body>
-        <div><span> </span><span id="info1b"></span></div>
 
+
+		<?php
+
+		    $s1 = array(array('Moins de 25 ans',$nbM25), array('Entre 25 et 50 ans',$nbM2550), array('Plus de 50 ans',$nbM50));
+		    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		    //Chart 1 Example
+		    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		    $pc = new C_PhpChartX(array($s1),'chart1');
+
+		    $pc->set_grid(array('drawBorder'=>false,
+					'drawGridlines'=>false,
+					'background'=>'#ffffff',
+					'shadow'=>false));
+		    $pc->set_axes_default(array());
+		    
+		    $pc->set_series_default(array(
+					'renderer'=>'plugin::PieRenderer',
+					'rendererOptions'=>array('showDataLabels'=>true)));
+		    $pc->set_legend(array('show'=>true,
+					'rendererOptions'=> array('numberRows'=> 1),
+					'location'=> 's'));
+		    $pc->draw(400,400);   
 
     ?>
-
-    </body>
-</html>
+	</div>
