2038958d8f124f13687c664f924b440319e25ce2
Antoine Perot <antoine.perot@etu.iut-tlse3.fr>
2016-06-04 18:39:04 +0200

    retouche stat

--- statistiques.php
+++ statistiques.php
@@ -4,68 +4,72 @@
 	<p id="textstat">Repartition demographique des patients</p>
 	<div id="divtableauStat">
 		<table id="tableauStat">
-			<tr>
-				<td>Tranches d'ages</td>
-				<td>Patient Masculin</td>
-				<td>Patient Feminin</td>
-			</tr>
-			<tr>
-				<td>Moins de 25 ans</td>
-				<td>
-					<?php $req=$linkpdo->query("Select count(*) as nbM25 from patient where civiliteP='M.' and DATEDIFF(CURDATE(), `dateNaissanceP`) < '9131.25'");
-						if($res=$req->fetch()) {
-							echo $res['nbM25'];
-							$nbM25 = $res['nbM25'];
-						}
-					?>
-				</td>
-				<td>
-					<?php $req=$linkpdo->query("Select count(*) as nbMme25 from patient where civiliteP='Mme.' and DATEDIFF(CURDATE(), `dateNaissanceP`) < '9131.25'");
-						if($res=$req->fetch()) {
-							echo $res['nbMme25'];
-							$nbMme25 =$res['nbMme25'];
-						}
-					?>
-				</td>
-			</tr>
-			<tr>
-				<td>Entre 25 et 50 ans</td>
-				<td>
-					<?php $req=$linkpdo->query("Select count(*) as nbM2550 from patient where civiliteP='M.' and DATEDIFF(CURDATE(), `dateNaissanceP`) > '9131.25' and DATEDIFF(CURDATE(), `dateNaissanceP`) < '18262.5'");
-						if($res=$req->fetch()) {
-							echo $res['nbM2550'];
-							$nbM2550 = $res['nbM2550'];
-						}
-					?>
-				</td>
-				<td>
-					<?php $req=$linkpdo->query("Select count(*) as nbMme2550 from patient where civiliteP='Mme.' and DATEDIFF(CURDATE(), `dateNaissanceP`) > '9131.25' and DATEDIFF(CURDATE(), `dateNaissanceP`) < '18262.5'");
-						if($res=$req->fetch()) {
-							echo $res['nbMme2550'];
-							$nbMme2550 = $res['nbMme2550'];
-						}
-					?>
-				</td>
-			</tr>	
-			<tr>
-				<td>Plus de 50 ans</td>
-				<td>
-					<?php $req=$linkpdo->query("Select count(*) as nbM50 from patient where civiliteP='M.' and DATEDIFF(CURDATE(), `dateNaissanceP`) > '18262.5'");
-						if($res=$req->fetch()) {
-							echo $res['nbM50'];
-							$nbM50 = $res['nbM50'];
-						}
-					?>
-				</td>
-				<td>
-					<?php $req=$linkpdo->query("Select count(*) as nbMme50 from patient where civiliteP='Mme.' and DATEDIFF(CURDATE(), `dateNaissanceP`) > '18262.5'");
-						if($res=$req->fetch()) {
-							echo $res['nbMme50'];
-							$nbMme50 = $res['nbMme50'];
-						}
-					?>
-				</td>
-			</tr>
+			<thead id="enteteTabStat">
+				<tr>
+					<td>Tranches d'ages</td>
+					<td>Patient Masculin</td>
+					<td>Patient Feminin</td>
+				</tr>
+			</thead>
+			<tbody id="donneesStat">
+				<tr>
+					<td>Moins de 25 ans</td>
+					<td>
+						<?php $req=$linkpdo->query("Select count(*) as nbM25 from patient where civiliteP='M.' and DATEDIFF(CURDATE(), `dateNaissanceP`) < '9131.25'");
+							if($res=$req->fetch()) {
+								echo $res['nbM25'];
+								$nbM25 = $res['nbM25'];
+							}
+						?>
+					</td>
+					<td>
+						<?php $req=$linkpdo->query("Select count(*) as nbMme25 from patient where civiliteP='Mme.' and DATEDIFF(CURDATE(), `dateNaissanceP`) < '9131.25'");
+							if($res=$req->fetch()) {
+								echo $res['nbMme25'];
+								$nbMme25 =$res['nbMme25'];
+							}
+						?>
+					</td>
+				</tr>
+				<tr>
+					<td>Entre 25 et 50 ans</td>
+					<td>
+						<?php $req=$linkpdo->query("Select count(*) as nbM2550 from patient where civiliteP='M.' and DATEDIFF(CURDATE(), `dateNaissanceP`) > '9131.25' and DATEDIFF(CURDATE(), `dateNaissanceP`) < '18262.5'");
+							if($res=$req->fetch()) {
+								echo $res['nbM2550'];
+								$nbM2550 = $res['nbM2550'];
+							}
+						?>
+					</td>
+					<td>
+						<?php $req=$linkpdo->query("Select count(*) as nbMme2550 from patient where civiliteP='Mme.' and DATEDIFF(CURDATE(), `dateNaissanceP`) > '9131.25' and DATEDIFF(CURDATE(), `dateNaissanceP`) < '18262.5'");
+							if($res=$req->fetch()) {
+								echo $res['nbMme2550'];
+								$nbMme2550 = $res['nbMme2550'];
+							}
+						?>
+					</td>
+				</tr>	
+				<tr>
+					<td>Plus de 50 ans</td>
+					<td>
+						<?php $req=$linkpdo->query("Select count(*) as nbM50 from patient where civiliteP='M.' and DATEDIFF(CURDATE(), `dateNaissanceP`) > '18262.5'");
+							if($res=$req->fetch()) {
+								echo $res['nbM50'];
+								$nbM50 = $res['nbM50'];
+							}
+						?>
+					</td>
+					<td>
+						<?php $req=$linkpdo->query("Select count(*) as nbMme50 from patient where civiliteP='Mme.' and DATEDIFF(CURDATE(), `dateNaissanceP`) > '18262.5'");
+							if($res=$req->fetch()) {
+								echo $res['nbMme50'];
+								$nbMme50 = $res['nbMme50'];
+							}
+						?>
+					</td>
+				</tr>
+			</tbody>
 		</table>
 	</div>
 	<table>
